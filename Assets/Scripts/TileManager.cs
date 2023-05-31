f82291f2766bebcae62a9e18a79651ce425dbdd4
Mike19200 <83485035+Mike19200@users.noreply.github.com>
2023-05-30 22:40:45 -0500

    Cambio PlayerController y Creacion Tile Manager

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    private List<GameObject> activeTiles = new List<GameObject>();
+    public GameObject[] tilePrefabs;
+
+    public float tileLength = 50;
+    public int numberOfTiles = 3;
+
+    public float zSpawn = 0;
+
+    public Transform playerTransform;
+
+    void Start()
+    {
+        for (int i = 0; i < numberOfTiles; i++)
+        {
+            if(i==0)
+                SpawnTile(0);
+            else
+                SpawnTile(Random.Range(0, tilePrefabs.Length));
+        }
+    }
+    void Update()
+    {
+        if(playerTransform.position.z - 50 >= zSpawn - (numberOfTiles * tileLength))
+        {
+            SpawnTile(Random.Range(0, tilePrefabs.Length));
+            DeleteTile();
+        }
+            
+    }
+
+    public void SpawnTile(int tileIndex)
+    {
+        GameObject tile = Instantiate(tilePrefabs[tileIndex], transform.forward * zSpawn, transform.rotation);
+        activeTiles.Add(tile);
+        zSpawn += tileLength;
+    }
+
+    private void DeleteTile()
+    {
+        activeTiles[0].SetActive(false);
+        activeTiles.RemoveAt(0);
+    }
+}
