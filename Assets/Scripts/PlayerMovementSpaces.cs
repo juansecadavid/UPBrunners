f9e75c9c253cc8d52d8cb9018ad78ba7db678f6c
mike <83485035+Mike19200@users.noreply.github.com>
2023-06-01 23:42:03 -0500

    carriles

---
+++ Assets/Scripts/PlayerMovementSpaces.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementSpaces : MonoBehaviour
+{
+    public float velocidadMovimiento = 5f;  // Velocidad de movimiento del jugador
+    public Transform carrilesParent;
+    public Transform[] carrilesPositions;
+
+    public Transform carrilPositionActual;
+
+    public int carrilActualIndex = 1; //carriles 0, 1 y 2
+
+    public float playerHorizontalSpeed=10f;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        carrilesPositions = new Transform[carrilesParent.childCount];
+        for (int i = 0; i < carrilesParent.childCount; i++)
+        {
+            carrilesPositions[i] = carrilesParent.GetChild(i);
+        }
+
+        carrilPositionActual = carrilesPositions[carrilActualIndex];
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        transform.Translate(Vector3.forward * velocidadMovimiento * Time.fixedDeltaTime,Space.World);
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            if (carrilActualIndex > 0)
+            {
+                carrilActualIndex--;            
+            }
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            if (carrilActualIndex < carrilesPositions.Length-1)
+            {
+                carrilActualIndex++;
+            }
+        }
+        carrilPositionActual = carrilesPositions[carrilActualIndex];
+
+        transform.position = Vector3.MoveTowards(transform.position,carrilPositionActual.position,playerHorizontalSpeed*Time.deltaTime);
+
+    }
+}
